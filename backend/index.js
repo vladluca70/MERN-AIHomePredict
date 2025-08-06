57b46b3e668584904f237c5d5bde288497149f1b
vladluca70 <vladluca70@gmail.com>
2025-08-06 23:08:19 +0300

    mongodb connection, schema and model created in backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,26 @@
 const express=require('express')
 const cors=require('cors')
+const mongoose=require('mongoose')
 const port=5000
-
 const server=express()
+const url='mongodb://localhost:27017/home-prediction';
+
+server.use(express.json())
+server.use(cors())
+
+mongoose.connect(url, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const userSchema = new mongoose.Schema({
+    username:{type:String, required:true},
+    password:{type:String, required:true}
+});
+const userModel=mongoose.model('User', userSchema)
+
+
+
 
 server.listen(
     port, ()=>{

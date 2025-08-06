20145c41f6b18767d21ce02ef1127c7653a0509e
vladluca70 <vladluca70@gmail.com>
2025-08-06 22:48:59 +0300

    authPage started

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react"
 
 
 function AuthPage()
 {
+    const [username, setUsername]=useState('')
 
+    function handleUsernameChange(e){
+        setUsername(e.target.value)
+    }
+
+    async function handleRegisterType() {
+        await handleRequest('register')
+    }
+
+    async function handleRequest(requestType) {
+        
+    }
+
+    return(
+        <>
+            <input onChange={(e)=>handleUsernameChange(e)}/>
+            <button onClick={handleRegisterType}>Register</button>
+        </>
+    )
 }
 
 export default AuthPage

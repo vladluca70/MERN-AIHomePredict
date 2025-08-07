5cf14513f8b2abe3c2d5e85095ff62c55d3e071d
vladluca70 <vladluca70@gmail.com>
2025-08-07 11:51:22 +0300

    predict price page- updated inputs && handle functions for onChange hook

--- frontend/src/PredictPricePage.jsx
+++ frontend/src/PredictPricePage.jsx
@@ -1,18 +1,56 @@
+import { useState } from "react"
 
 
 function PredictPricePage({username})
 {
+    const [numberOfBedrooms, setNumberOfBedrooms]=useState('')
+    const [numberOfBathrooms, setNumberOfBathrooms]=useState('')
+    const [livingAreaSize, setLivingAreaSize]=useState('')
+    const [totalLotSize, setTotalLotSize]=useState('')
+    const [numberOfFloors, setNumberOfFloors]=useState('')
+    const [hasWaterfrontView, setHasWaterFrontView]=useState('')
+    const [qualityLevelOfPropertyView, setQualityLevelOfPropertyView]=useState('')
+
+    function handleNumberOfBedrooms(e){
+        setNumberOfBedrooms(e.target.value);
+    }
+
+    function handleNumberOfBathrooms(e){
+        setNumberOfBathrooms(e.target.value);
+    }
+
+    function handleLivingAreaSize(e){
+        setLivingAreaSize(e.target.value);
+    }
+
+    function handleTotalLotSize(e){
+        setTotalLotSize(e.target.value);
+    }
+
+    function handleNumberOfFloors(e){
+        setNumberOfFloors(e.target.value);
+    }
+
+    function handleHasWaterFrontView(e){
+        setHasWaterFrontView(e.target.value);
+    }
+
+    function handleQualityLevelOfPropertyView(e){
+        setQualityLevelOfPropertyView(e.target.value);
+    }
+
     return(
         <>
         <p>Hello {username}</p> <br/>
 
-        Number of bedrooms: <input placeholder="A number between 0 and 10"/><br/>
-        Number of bathrooms: <input placeholder="A number between 0 and 10"/><br/>
-        Living area size in square feet: <input placeholder="A number between 100 and 5000"/><br/>
-        Total lot size in square feet: <input placeholder="A number between 100 and 40k"/><br/>
-        Number of floors: <input placeholder="A number between 1 and 5"/><br/>
-        Indicates if property has waterfront view: <input placeholder="0 for no, 1 for yes"/><br/>
-        Quality level of property view: <input placeholder="A number between 0 and 5"/>
+        Number of bedrooms: <input type="number" onChange={(e)=>handleNumberOfBedrooms(e)} placeholder="A number between 0 and 10" required/><br/>
+        Number of bathrooms: <input type="number" onChange={(e)=>handleNumberOfBathrooms(e)} placeholder="A number between 0 and 10" required/><br/>
+        Living area size in square feet: <input type="number" onChange={(e)=>handleLivingAreaSize(e)} placeholder="A number between 100 and 5000" required/><br/>
+        Total lot size in square feet: <input type="number" onChange={(e)=>handleTotalLotSize(e)} placeholder="A number between 100 and 40k" required/><br/>
+        Number of floors: <input type="number" onChange={(e)=>handleNumberOfFloors(e)} placeholder="A number between 1 and 5" required/><br/>
+        Indicates if property has waterfront view: <input type="number" onChange={(e)=>handleHasWaterFrontView(e)} placeholder="0 for no, 1 for yes" required/><br/>
+        Quality level of property view: <input type="number" onChange={(e)=>handleQualityLevelOfPropertyView(e)} placeholder="A number between 0 and 5" required/> <br/>
+        <button>Send data</button>
         </>
     )
 }

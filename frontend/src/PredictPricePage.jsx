1bf2622a5c70ddcde56649dff9ad902763d5a290
vladluca70 <vladluca70@gmail.com>
2025-08-07 12:19:45 +0300

    predict price page- new inputs

--- frontend/src/PredictPricePage.jsx
+++ frontend/src/PredictPricePage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 
 function PredictPricePage({username})
 {
+    const [errorMessage, setErrorMessage]=useState('')
     const [numberOfBedrooms, setNumberOfBedrooms]=useState('')
     const [numberOfBathrooms, setNumberOfBathrooms]=useState('')
     const [livingAreaSize, setLivingAreaSize]=useState('')
@@ -10,6 +11,9 @@ function PredictPricePage({username})
     const [numberOfFloors, setNumberOfFloors]=useState('')
     const [hasWaterfrontView, setHasWaterFrontView]=useState('')
     const [qualityLevelOfPropertyView, setQualityLevelOfPropertyView]=useState('')
+    const [overallConditionRating, setOverallConditionRating]=useState('')
+    const [yearPropertyWasBuilt, setYearPropertyWasBuilt]=useState('')
+    const [yearPropertyWasLastRenovated, setYearPropertyWasLastRenovated]=useState('')
 
     function handleNumberOfBedrooms(e){
         setNumberOfBedrooms(e.target.value);
@@ -39,6 +43,26 @@ function PredictPricePage({username})
         setQualityLevelOfPropertyView(e.target.value);
     }
 
+    function handleOverallConditionRating(e){
+        setOverallConditionRating(e.target.value);
+    }
+
+    function handleYearPropertyWasBuilt(e){
+        setYearPropertyWasBuilt(e.target.value);
+    }
+
+    function handleYearPropertyWasLastRenovated(e){
+        setYearPropertyWasLastRenovated(e.target.value);
+    }
+    function sendAllDataFunction(){
+        if(!numberOfBedrooms || !numberOfBathrooms || !livingAreaSize || !totalLotSize
+            || !numberOfFloors || !hasWaterfrontView || !qualityLevelOfPropertyView || !overallConditionRating
+            || !yearPropertyWasBuilt || !yearPropertyWasLastRenovated){
+            return setErrorMessage("All fields must be completed")
+        }
+
+    }
+
     return(
         <>
         <p>Hello {username}</p> <br/>
@@ -50,7 +74,13 @@ function PredictPricePage({username})
         Number of floors: <input type="number" onChange={(e)=>handleNumberOfFloors(e)} placeholder="A number between 1 and 5" required/><br/>
         Indicates if property has waterfront view: <input type="number" onChange={(e)=>handleHasWaterFrontView(e)} placeholder="0 for no, 1 for yes" required/><br/>
         Quality level of property view: <input type="number" onChange={(e)=>handleQualityLevelOfPropertyView(e)} placeholder="A number between 0 and 5" required/> <br/>
-        <button>Send data</button>
+        
+        Overall condition rating: <input type="number" onChange={(e)=>handleOverallConditionRating(e)} placeholder="A number between 0 and 5" required/> <br/>
+        Year property was built: <input type="number" onChange={(e)=>handleYearPropertyWasBuilt(e)} placeholder="A number between 0 and 5" required/> <br/>
+        Year property was last renovated: <input type="number" onChange={(e)=>handleYearPropertyWasLastRenovated(e)} placeholder="A number between 0 and 5" required/> <br/>
+
+        <button onClick={sendAllDataFunction}>Send data</button> <br/>
+        {errorMessage && <p>{errorMessage}</p>}
         </>
     )
 }
--- house-price-model/create-model/create_model.py
+++ house-price-model/create-model/create_model.py
@@ -3,7 +3,13 @@ from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('Housing.csv')
 
-X = df[['bedrooms']]    # trebuie 2D
+A = df[['bedrooms']]    
+B = df[['bathrooms']]
+C = df[['sqft_living']]
+D = df[['sqft_lot']]
+E = df[['floors']]
+F = df[['waterfront']]
+G = df[['view']]
 y = df['price']     # target
 
 model = LinearRegression()
